c7c8bb7e26d60f4a9e26fb01f22a7a43a768ea74
sot4k <sot4kpg@gmail.com>
2023-01-01 17:06:41 +0900

    latest version

--- app.py
+++ app.py
@@ -12,6 +12,58 @@ from linebot.models import (
 
 import os
 
+
+import pandas as pd
+
+from datetime import datetime
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def auth():
+    SP_CREDENTIAL_FILE = 'secretkey.json'
+    SP_SCOPE = [
+        'https://spreadsheets.google.com/feeds', 
+        'https://www.googleapis.com/auth/drive'
+    ]
+    SP_SHEET_KEY = '17gkxQa1jSWTNLBcsygTARoMX4T0rL1HODAI9auxJ3sY'
+    SP_SHEET = 'timesheet' 
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
+    gc = gspread.authorize(credentials)
+
+    worksheet = gc.open_by_key(SP_SHEET_KEY).worksheet(SP_SHEET)
+    return worksheet
+
+
+#　出勤
+def punch_in():
+    worksheet = auth()
+    df = pd.DataFrame(worksheet.get_all_records())
+
+    timestamp = datetime.now()
+    date = timestamp.strftime('%Y/%m/%d')
+    punch_in = timestamp.strftime('%H:%M')
+
+    data = [[date, punch_in, '00:00']]
+    df1 = pd.DataFrame(data, columns = ['日付', '出勤時間', '退勤時間'])
+    df = pd.concat([df, df1])
+
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    print('勤怠（出勤）登録完了しました')
+
+#　退勤
+def punch_out():
+    worksheet = auth()
+    df = pd.DataFrame(worksheet.get_all_records())
+
+    timestamp = datetime.now()
+    punch_out = timestamp.strftime('%H:%M')
+
+    df.iloc[-1, 2] = punch_out
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    print('勤怠（退勤）登録完了しました')
+    
+    
 app = Flask(__name__)
 
 YOUR_CHANNEL_ACCESS_TOKEN = 'eVQVwlnnybvY62jzq8E+u5j0ql5+TunFFy3H1qw/E4Y7nryZL7TLUhfUsOFrYjLw8qaHCdPieNuVE4mGGXY+EgE4ZOq8vLmgAg5o54nG09JmoAJA2psjI0fiKE57kF8sdeu2IWar7grxEZY68uVqzgdB04t89/1O/w1cDnyilFU='
@@ -46,9 +98,22 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if event.message.text == "出勤":
+        punch_in()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='出勤登録完了しました！'))
+
+    elif event.message.text == "退勤":
+        punch_out()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='退勤登録完了しました！'))
+    
+    else: 
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='こちらは出退勤を管理するボットです'))
 
 
 if __name__ == "__main__":
